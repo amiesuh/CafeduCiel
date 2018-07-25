a1a93d8c6f09b0982731307c393cd295140fb3a9
Mansi Gandhi <mansimg@gmail.com>
2018-07-24 22:14:01 -0700

    inserting final files

--- businessproject/js/contact.js
+++ businessproject/js/contact.js
@@ -1,3 +1,5 @@
+var map;
+var center;
 function initMap() {
   var NewYork = {lat: 40.765511, lng: -73.969965};
   var SanFrancisco = {lat:37.787323, lng: -122.405354};
@@ -7,44 +9,75 @@ function initMap() {
   var Miami = {lat: 25.794756, lng: -80.223696};
   var Seattle = {lat: 47.608026, lng: -122.339108};
   var WashDC = {lat: 38.911539, lng: -77.031622}
-  var center = {lat: 39.828420, lng: -98.579490};
-  var map = new google.maps.Map(document.getElementById("map"), {
+  center = {lat: 39.828420, lng: -98.579490};
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: center
   });
+  for(var i )
   var NewYork = new google.maps.Marker({
     position: NewYork,
+    animation: google.maps.Animation.DROP,
     map: map
   });
   var SanFrancisco = new google.maps.Marker({
     position: SanFrancisco,
+    animation: google.maps.Animation.DROP,
     map:map
   });
   var LosAngeles = new google.maps.Marker({
     position: LosAngeles,
+    animation: google.maps.Animation.DROP,
     map:map
   });
   var Dallas = new google.maps.Marker({
     position: Dallas,
+    animation: google.maps.Animation.DROP,
     map: map
   });
   var Chicago = new google.maps.Marker({
     position: Chicago,
+    animation: google.maps.Animation.DROP,
     map: map
   });
   var Miami = new google.maps.Marker({
     position: Miami,
+    animation: google.maps.Animation.DROP,
     map: map
   });
   var Seattle = new google.maps.Marker({
     position: Seattle,
+    animation: google.maps.Animation.DROP,
     map: map
   });
   var WashDC = new google.maps.Marker({
     position: WashDC,
+    animation: google.maps.Animation.DROP,
     map: map
   });
+  var directory = ["Los Angeles: 1450-B 3rd Street Promenade, Santa Monica, CA 90401",
+                  "San Francisco: 40 Grant Ave, San Francisco, CA 94133",
+                  "New York: 690 Madison Ave, New York, NY 10065",
+                  "Dallas: 1400 Elm St #3750, Dallas, TX 75202",
+                  "Chicago: 520 N Clark St, Chicago, IL 60654",
+                  "Miami: 1720 NW 20th St, Miami, FL 33142",
+                  "Seattle: 1400 1st Ave, Seattle, WA 98101",
+                  "Washington DC: 1601 14th St NW, Washington, DC 20009"];
+  var names = ["New York", "Los Angeles","San Francisco", "Dallas", "Chicago", "Miami", "Seattle", "Washington DC"];
+  var markers = [NewYork, LosAngeles, SanFrancisco, Dallas, Chicago, Miami, Seattle, WashDC];
+  for(var x = 0; x < names.length; x++){
+    var name = names[x];
+    var contentString = directory[x];
 
+    var infowindow = new google.maps.InfoWindow({
+        title: name
+        content: contentString
+    });
+
+    google.maps.event.addListener(markers[x], 'click', function() {
+      infowindow.open(map, markers[x]);
+    });
+  }
 }
 function findLocation(){
   var directory = ["Los Angeles: 1450-B 3rd Street Promenade, Santa Monica, CA 90401",
@@ -65,7 +98,11 @@ function findLocation(){
   // var locationAddress = document.getElementById("address")
   if(zip > 00000 && zip < 99999){
     if(zip >= 00000 && zip < 12500){
-      alert("Your nearest location is located in New York. The address is \n" + directory[2]);
+      map.setCenter(new google.maps.LatLng(40.765511, -73.969965));
+      map.setZoom(12);
+
+      // map.panTo(NewYork);÷\
+      //alert("Your nearest location is located in New York. The address is \n" + directory[2]);
     }
     else if(zip >= 12500 && zip < 25000){
       alert("Your nearest location is located in Washington DC. The address is \n" + directory[7]);
